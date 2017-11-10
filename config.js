d5d27760b0642cbe93a533743d09ed5025e61b94
thekoushik <thekoushik.universe@gmail.com>
2017-11-10 16:45:17 +0530

    config.js removed from gitignore

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports={
+    mongoURI:'mongodb://127.0.0.1/express-starter',
+    email:{
+        service: "gmail",
+        auth: {
+            user: "youremail@gmail.com",
+            pass: "yourpassword"
+        }
+    },
+    url: 'http://localhost:8000'
+}
