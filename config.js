6b9d020cd433ad9a187281f0749349b49eb0422a
thekoushik <thekoushik.universe@gmail.com>
2018-09-20 17:59:00 +0530

    redis config added in config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports={
     mongoURI:'mongodb://127.0.0.1/express-starter',
+    redis:{
+        host: '127.0.0.1',
+        port: 6379
+    },
     email:{
         service: "gmail",
         auth: {
